4499132bf598831a7b065eb918c8d45b4c4ad8c7
Michael Syson <masarapmabuhay@gmail.com>
2017-08-02 19:45:26 +0800

    +updated: SQL DB --> added: Adarna House, Inc. as merchant --> added: children's (books) category --> added: new children's (books) product items

---
+++ db/usbong_storeInsertProductTypeOnly20170802.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 02, 2017 at 01:31 PM
+-- Server version: 10.1.22-MariaDB
+-- PHP Version: 7.1.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `usbong_store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_type`
+--
+/*
+CREATE TABLE `product_type` (
+  `product_type_id` int(11) NOT NULL,
+  `product_type_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+*/
+--
+-- Dumping data for table `product_type`
+--
+
+INSERT INTO `product_type` (`product_type_id`, `product_type_name`) VALUES
+(1, 'All'),
+(2, 'Books'),
+(3, 'Beverages'),
+(4, 'Books/Beverages'),
+(5, 'Promos'),
+(6, 'Comics'),
+(7, 'Manga'),
+(8, 'Toys & Collectibles'),
+(9, 'Textbooks'),
+(10, 'Children\'s');
+
+--
+-- Indexes for dumped tables
+--
+/*
+--
+-- Indexes for table `product_type`
+--
+ALTER TABLE `product_type`
+  ADD PRIMARY KEY (`product_type_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `product_type`
+--
+ALTER TABLE `product_type`
+  MODIFY `product_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+*/  
+  
+  COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
