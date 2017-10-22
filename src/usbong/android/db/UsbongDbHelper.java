699bd10db9f24590ca5e2f7250754e9e018ed157
Michael Syson <masarapmabuhay@gmail.com>
2017-10-22 14:15:27 +0800

    +added: product items from the Usbong Store (web) --> TODO: update: background image of select images from white to black

--- src/usbong/android/db/UsbongDbHelper.java
+++ src/usbong/android/db/UsbongDbHelper.java
@@ -36,7 +36,7 @@ import android.provider.BaseColumns;
 
 public class UsbongDbHelper extends SQLiteOpenHelper {
 	// If you change the database schema, you must increment the database version.
-    public static final int DATABASE_VERSION = 8;
+    public static final int DATABASE_VERSION = 9;
 //    public static final String DB_NAME = "usbong_store.db";
     
 //    private static String DB_DIR = "/data/data/usbong.android.store_app/databases/";
